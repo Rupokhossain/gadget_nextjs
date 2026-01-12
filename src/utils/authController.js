e0128e9ebdaf90f5fabaa43e38b825d9d72b262d
Siam Ahmed <siamahmed.only@gmail.com>
2026-01-12 17:52:38 +0600

    update

--- src/utils/authController.js
+++ src/utils/authController.js
@@ -3,12 +3,13 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
 };
+
