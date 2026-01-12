d620b8b4b7085a7e1a59d419e19a2e51c97bad1a
Siam Ahmed <siamahmed.only@gmail.com>
2026-01-12 18:07:50 +0600

    update

--- src/app/HotDeals/HotDeals.jsx
+++ src/app/HotDeals/HotDeals.jsx
@@ -14,6 +14,7 @@ import { CiHeart } from "react-icons/ci";
 
 import AOS from "aos";
 import { useProductActions } from "@/hooks/useProductActions";
+import { useSelector } from "react-redux";
 
 const HotDeals = ({ phones }) => {
   const { handleAddToCart, handleWishlistAction } = useProductActions();
